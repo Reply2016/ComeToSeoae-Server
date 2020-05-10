ec8009030d41e68cbaa4126d4d50c6383fa47cb1
jonggeunpark <53558710+jonggeunpark@users.noreply.github.com>
2020-05-11 02:58:11 +0900

    Update pool.js

--- module/pool.js
+++ module/pool.js
@@ -6,13 +6,13 @@ module.exports = {
         let result
         const pool = await poolPromise;
         try {
-            var connection = await pool.getConnection() // connection을 pool에서 하나 가져온다.
-            result = await connection.query(query) || null // query문의 결과 || null 값이 result에 들어간다.
+            var connection = await pool.getConnection()
+            result = await connection.query(query) || null
         } catch (err) {
             console.log(err)
             connection.rollback(() => { })
         } finally {
-            pool.releaseConnection(connection) // waterfall 에서는 connection.release()를 사용했지만, 이 경우 pool.releaseConnection(connection) 을 해준다.
+            pool.releaseConnection(connection)
             return result
         }
     },
@@ -20,14 +20,16 @@ module.exports = {
         const query = args[0]
         const value = args[1] // array
         let result
+        const pool = await poolPromise;
         try {
-            var connection = await pool.getConnection() // connection을 pool에서 하나 가져온다.
-            result = await connection.query(query, value) || null // 두 번째 parameter에 배열 => query문에 들어갈 runtime 시 결정될 value
+            var connection = await pool.getConnection()
+            result = await connection.query(query, value) || null
         } catch (err) {
+            console.log(err)
             connection.rollback(() => { })
             next(err)
         } finally {
-            pool.releaseConnection(connection) // waterfall 에서는 connection.release()를 사용했지만, 이 경우 pool.releaseConnection(connection) 을 해준다.
+            pool.releaseConnection(connection)
             return result
         }
     },
